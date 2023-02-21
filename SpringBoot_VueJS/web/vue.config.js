909acf009d704c3be1115097b32d7f8781a7caba
smw0807 <smw0807@gmail.com>
2023-02-21 22:51:57 +0900

    vue-cli 빌드 배포 위치 수정 및 빌드된 파일

--- SpringBoot_VueJS/web/vue.config.js
+++ SpringBoot_VueJS/web/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //빌드 시 결과물 내보낼 디렉터리 위치
+  outputDir: '../src/main/resources/static',
+  //개발모드 진행시 포트가 다르기 때문에 프록시 설정
+  devServer: {
+    proxy: 'http://localhost:8080'
+  }
 })
