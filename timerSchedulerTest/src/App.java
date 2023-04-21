9d70a7f01096890dc1c0b609141620bc28f23d63
Minu Song <smw0807@gmail.com>
2023-04-22 02:56:58 +0900

    소스 수정 쉘에서 시작, 킬 처리해서 자바 소스 간략화

--- timerSchedulerTest/src/App.java
+++ timerSchedulerTest/src/App.java
@@ -1,45 +1,25 @@
 import java.util.TimerTask;
 import java.util.Timer;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class App {
-  private static ScheduledExecutorService scheduler;
   private Timer timer;
-
   public App() {
       timer = new Timer();
   }
 
-
-  private static void startScheduler() {
-    scheduler = Executors.newSingleThreadScheduledExecutor();
-    scheduler.scheduleAtFixedRate(App::Run, 0, 5, TimeUnit.SECONDS);
-    System.out.println("Scheduler started");
-  }
-
-  private static void stopScheduler() {
-    if (scheduler != null) {
-      scheduler.shutdown();
-      System.out.println("Scheduler stopped");
-    }
-  }
-
-  public static void Run() {
-    System.out.println("Run!!!");
+  public void RunSchedule() {
+    TimerTask task = new TimerTask(){
+      @Override
+      public void run() {
+        System.out.println("RUN!!");
+      }
+    };
+    timer.scheduleAtFixedRate(task, 0, 5 * 1000);
   }
 
   public static void main(String[] args) throws Exception {
     System.out.println("Hello World!!");
-    String mode = args[0];
-    System.out.println("MODE : " + mode);
-    if ("START".equals(mode)) {
-      startScheduler();
-    } else if ("STOP".equals(mode)) {
-      stopScheduler();
-    } else {
-      throw new Error("mode check");
-    }
+    App app = new App();
+    app.RunSchedule();
   }
 }
